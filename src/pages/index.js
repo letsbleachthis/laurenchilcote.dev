81c9fa823faf1eca7a6af01de8692214659a1293
Lauren Chilcote <laurenchilcote@Laurens-MacBook-Pro-3.local>
2019-10-28 20:54:00 -0400

    Remove commented out img, adding first project section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,20 @@ import "../utils/App.css"
 import Layout from "../components/layout/layout"
 import Header from "../components/header/header"
 
+import storiesHero from "../images/instagram-stories/stories.png"
+
 export default () => (
 <Layout>
     <Header headerText="Hi! I’m Lauren, a Product Designer &amp; Front-End Developer."/>
     <p>I craft thoughtful solutions to user problems and work cross-functionally to drive products from ideation through implementation.</p>
-    {/* <img src="https://source.unsplash.com/random/400x200" alt=""/> */}
+
     <Link to="/about">The nitty gritty &#10230;</Link>
+
+    <div style={{margin: "3rem auto", display: "flex", flexDirection: "column", alignItems: "center center", textAlign: "center"}}>
+        <h2>Plan, preview, and schedule Instagram Stories with Buffer Publish.</h2>
+        <p>Product Design, UX Research</p>
+        <img src={storiesHero} alt="Plan, preview, and schedule Instagram Stories with Buffer Publish." />
+        <button value="More about Instagram Stories">More about Instagram Stories</button>
+    </div>
 </Layout>
 )
