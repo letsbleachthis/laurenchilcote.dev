d914814703ddb10df83c3479ab8d6052f658a7b5
Lauren Chilcote <laurenchilcote@Laurens-MacBook-Pro-3.local>
2019-10-28 20:51:38 -0400

    Adding footer to layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, Link, graphql  } from "gatsby"
 import layoutStyles from "./layout.module.css"
+import portrait from "../../images/lauren-chilcote.jpeg"
 
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -34,6 +35,15 @@ export default ({ children }) => {
       </ul>
     </header>
     {children}
+    <footer className={layoutStyles.footer}>
+      <div className={layoutStyles.copyContainer}>
+        <p>Lauren Chilcote</p>
+        <p>Curious by nature and keen on collaboration, I believe there is a multitude of success to be found at the intersection between business goals and user problems when individuals band together to work as a team. I’m currently working with the fine folks at Buffer to design and build software that helps innovative companies grow their brands online.</p>
+        <p>When not in front of my computer, you can find me advocating for the existence of the cake flight, powerlifting with The Armory barbell club, or staring at/petting/fawning over someone’s dog.</p>
+      </div>
+      <img src={portrait} alt="A portrait of Lauren Chilcote"/>
+    </footer>
   </div>
 )
 }
+
