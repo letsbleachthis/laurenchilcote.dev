3ed423f56717589d853b95f2db4dc6b6edcb2630
Lauren Chilcote <laurenchilcote@Laurens-MacBook-Pro-3.local>
2019-10-29 20:54:07 -0400

    Adding new portrait img, styling on footer

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { useStaticQuery, Link, graphql  } from "gatsby"
+
 import layoutStyles from "./layout.module.css"
-import portrait from "../../images/lauren-chilcote.jpeg"
+import "../../utils/App.css"
+import portrait from "../../images/img_lauren_chilcote_crop.jpeg"
 
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -37,11 +39,17 @@ export default ({ children }) => {
     {children}
     <footer className={layoutStyles.footer}>
       <div className={layoutStyles.copyContainer}>
-        <p>Lauren Chilcote</p>
-        <p>Curious by nature and keen on collaboration, I believe there is a multitude of success to be found at the intersection between business goals and user problems when individuals band together to work as a team. I’m currently working with the fine folks at Buffer to design and build software that helps innovative companies grow their brands online.</p>
-        <p>When not in front of my computer, you can find me advocating for the existence of the cake flight, powerlifting with The Armory barbell club, or staring at/petting/fawning over someone’s dog.</p>
+        <p><strong>Lauren Chilcote</strong></p>
+        <p>Curious by nature and keen on collaboration, I believe there is a multitude of success to be found at the intersection between business goals and user problems when individuals band together to work as a team.</p>
+        <p className='finePrint'>Currently working with the fine folks at Buffer to help innovative companies grow their brands online.</p>
+        {/* <p>When not in front of my computer, you can find me advocating for the existence of the cake flight, powerlifting with The Armory barbell club, or staring at/petting/fawning over someone’s dog.</p> */}
+        <ul className={layoutStyles.socialLinks}>
+          <li className={layoutStyles.twitter}><a href="https://twitter.com/LaurenChilcote" target="_blank">Twitter</a></li>
+          <li className={layoutStyles.linkedIn}><a href="https://www.linkedin.com/in/laurenchilcote/" target="_blank">LinkedIn</a></li>
+          <li className={layoutStyles.gitHub}><a href="https://github.com/letsbleachthis" target="_blank">GitHub</a></li>
+        </ul>
       </div>
-      <img src={portrait} alt="A portrait of Lauren Chilcote"/>
+      <img src={portrait} alt="A portrait of Lauren Chilcote" className={layoutStyles.portrait}/>
     </footer>
   </div>
 )
