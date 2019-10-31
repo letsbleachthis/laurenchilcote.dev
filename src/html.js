7c8174a92e08e28857df794142aa209cf0752904
letsbleachthis <chilcote.lauren@gmail.com>
2019-10-31 17:56:47 -0400

    Minor spacing and copy tweaks

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        
         <script src="https://use.fontawesome.com/ca9de9b6bc.js"></script>
       </head>
       <body {...props.bodyAttributes}>
