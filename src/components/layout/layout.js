c7c2b2c13dc2e52c057cb030dbc69377caf3f778
letsbleachthis <chilcote.lauren@gmail.com>
2019-10-31 17:56:18 -0400

    Playing with colors, editing header and footer text and layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,8 +1,11 @@
 import React from "react"
 import { useStaticQuery, Link, graphql  } from "gatsby"
+import "typeface-poppins"
+import "typeface-open-sans"
 
-import "./layout.css"
+import "../../../node_modules/normalize.css/normalize.css"
 import layoutStyles from "./layout.module.css"
+import "./layout.css"
 import portrait from "../../images/img_lauren_chilcote_crop.jpeg"
 
 const ListLink = props => (
@@ -25,7 +28,7 @@ export default ({ children }) => {
   )
   return (
   <div className={layoutStyles.layoutContainer}>
-    <header className={layoutStyles.pageTitle}>
+    <header className={layoutStyles.mainNav}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 className={layoutStyles.siteTitle}>{data.site.siteMetadata.title}</h3>
       </Link>
@@ -38,34 +41,34 @@ export default ({ children }) => {
     </header>
     {children}
     <footer className={layoutStyles.footer}>
-      <div className={layoutStyles.copyContainer}>
-        <p><strong>Lauren Chilcote</strong></p>
-        <p>Curious by nature and keen on collaboration, I believe there is a multitude of success to be found at the intersection between business goals and user problems when individuals band together to work as a team.</p>
-        <p className='finePrint'>Currently working with the fine folks at Buffer to help innovative companies grow their brands online.</p>
-        {/* <p>When not in front of my computer, you can find me advocating for the existence of the cake flight, powerlifting with The Armory barbell club, or staring at/petting/fawning over someone’s dog.</p> */}
-        <ul className={layoutStyles.socialLinks}>
-          <li className={`${layoutStyles.iconContainer} ${layoutStyles.twitter}`}>
-            <a className={layoutStyles.socialIcon} href="https://twitter.com/LaurenChilcote" target="_blank">
-              <i className="fa fa-lg fa-twitter"></i>
-              
-            </a>
-          </li>
-          <li className={`${layoutStyles.iconContainer} ${layoutStyles.linkedIn}`}>
-            <a className={layoutStyles.socialIcon} href="https://www.linkedin.com/in/laurenchilcote/" target="_blank">
-              <i className="fa fa-lg fa-linkedin"></i>
-              
-            </a>
-          </li>
-          <li className={`${layoutStyles.iconContainer} ${layoutStyles.gitHub}`}>
-            <a className={layoutStyles.socialIcon} href="https://github.com/letsbleachthis" target="_blank">
-              <i className="fa fa-lg fa-github"></i>
-              
-            </a>
-          </li>
-          <li><Link to="/about/">Find out more <i className="fa fa-long-arrow-right"></i></Link></li>
-        </ul>
-      </div>
-      <img src={portrait} alt="A portrait of Lauren Chilcote" className={layoutStyles.portrait}/>
+    <div className={layoutStyles.copyContainer}>
+          <h4>Lauren Chilcote</h4>
+          <p>Digital Product Designer, powerlifter, and doggo enthusiast. Currently working with the fine folks at Buffer to help innovative companies grow their brands online.</p>
+          <ul className={layoutStyles.socialLinks}>
+            <li className={`${layoutStyles.iconContainer} ${layoutStyles.twitter}`}>
+              <a className={layoutStyles.socialIcon} href="https://twitter.com/LaurenChilcote" target="_blank">
+                <i className="fa fa-lg fa-twitter"></i>
+                
+              </a>
+            </li>
+            <li className={`${layoutStyles.iconContainer} ${layoutStyles.linkedIn}`}>
+              <a className={layoutStyles.socialIcon} href="https://www.linkedin.com/in/laurenchilcote/" target="_blank">
+                <i className="fa fa-lg fa-linkedin"></i>
+                
+              </a>
+            </li>
+            <li className={`${layoutStyles.iconContainer} ${layoutStyles.gitHub}`}>
+              <a className={layoutStyles.socialIcon} href="https://github.com/letsbleachthis" target="_blank">
+                <i className="fa fa-lg fa-github"></i>
+                
+              </a>
+            </li>
+            <li><Link to="/about/">Find out more <i className="fa fa-long-arrow-right"></i></Link></li>
+          </ul>
+        </div>
+        <div className={layoutStyles.portraitContainer}>
+        <img src={portrait} alt="A portrait of Lauren Chilcote" className={layoutStyles.portrait}/>
+        </div>
     </footer>
   </div>
 )
