8c6156f4777ef2e81a5b5ed806cd3257b96bb4e1
letsbleachthis <chilcote.lauren@gmail.com>
2019-11-03 16:00:51 -0500

    Tidy up front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import Header from "../components/header/header"
 
-import storiesHero from "../images/instagram-stories/stories.png"
-
 export default () => (
 <Layout>
-    <Header headerText="Hi! I’m Lauren, a Product Designer &amp; Front-End Developer."/>
-    <p>I craft thoughtful solutions to user problems and work cross-functionally to drive products from ideation through implementation. <Link to="/about">The nitty gritty &#10230;</Link></p>
-    
-
-    <div style={{margin: "3rem auto", display: "flex", flexDirection: "column", alignItems: "center center", textAlign: "center"}}>
-        <p style={{textTransform: "uppercase", color: "grey", fontSize: "14px"}}>Featured Work</p>
-        <h2>Plan, preview, and schedule Instagram Stories with Buffer Publish.</h2>
-        <p>Product Design, UX Research</p>
-        <img style={{width: "100%"}} src={storiesHero} alt="Plan, preview, and schedule Instagram Stories with Buffer Publish." />
-        <button value="More about Instagram Stories">More about Instagram Stories</button>
+    <div className="section sectionPrimary">
+        <div className="wrapper">
+            <Header headerText="Hi! I’m Lauren, a Product Designer &amp; Front-End Developer."/>
+            <p>I craft thoughtful solutions to user problems and work cross-functionally to drive products from ideation through implementation. <Link to="/about">The nitty gritty &#10230;</Link></p>
+        </div>
     </div>
 </Layout>
 )
